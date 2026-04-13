ee50df7729412fa836c74167e6c52e53b3e390b5
Coen Büller <c.m.buller@student.tudelft.nl>
2026-04-13 22:33:33 +0200

    Put everything in a audio processing class and now it looks nicer and work, easier to understand and more flexable

--- final-project/AudioStream.py
+++ final-project/AudioStream.py
@@ -2,38 +2,50 @@ import sounddevice as sd
 import numpy as np
 import queue 
 import librosa as lb
+import argparse
+import matplotlib.pyplot as plt
 
-from MFCC import CalcMFCC
+from matplotlib.animation import FuncAnimation
+from audioProcessor import AudioProcessor
+parser = argparse.ArgumentParser()
+parser.add_argument("--d", type=int, help="Decide which device you want to record with (int)")
+parser.add_argument("--samplerate", type=int, help="Devine the sample rate at which you want to record your audio (int)", default=4000)
+parser.add_argument("--channels", type=int, help="Number of channels (int)", default=1)
+parser.add_argument("--blocksize", type=int, help="Blocksize at which you want to process the sounddata", default=256)
+parser.add_argument("--plot", type=bool, help="Do you want to plot the frequency intesities", default=False)
 
-sd.default.samplerate = 4_000
-sd.default.channels = 4
-sd.default.blocksize = 256
+
+args = parser.parse_args()
+device_idx = args.d
+
+sd.default.samplerate = args.samplerate
+sd.default.channels = args.channels
+sd.default.blocksize = args.blocksize
 sd.default.dtype = np.int16 
 
 audio_queue = queue.Queue()
-hanningWindow = np.hanning(sd.default.blocksize)
-melFilters = lb.filters.mel(sr=sd.default.samplerate, n_fft=sd.default.blocksize*2-1, n_mels=13)
-n_fft_internal = sd.default.blocksize * 2 - 1
-x_fft = np.fft.rfftfreq(n_fft_internal, 1/sd.default.samplerate)
+ap = AudioProcessor(blocksize=args.blocksize, samplerate=args.samplerate)
+
+fig, ax = plt.subplots()
+line, = ax.plot(ap.x_fft, np.zeros(len(ap.x_fft)))
+vline = ax.axvline(x=0, color='r', linestyle='--')
+
+ax.set_ylim(0, 100000) # Adjust based on expected volume
+ax.set_title("Live FFT")
+ax.set_xlabel("Frequency (Hz)")
 
 def callback(indata, frames, time, status):
     audio_queue.put(indata.copy()) # Puts data into queue
 
-stream = sd.InputStream(device=1, channels=1, callback=callback)
+stream = sd.InputStream(device=device_idx, channels=1, callback=callback)
 with stream:
     print('listening')
     try:
         while True:
             soundData = None
-            while not audio_queue.empty():
-                soundData = audio_queue.get()
-
-                mfcc, pitch = CalcMFCC(soundData=soundData,
-                                       hanningWindow=hanningWindow,
-                                       melFilters=melFilters,
-                                       x_fft=x_fft)
-                
-                print(pitch)
+            ani = FuncAnimation(fig, ap.update_plot, fargs=(line, vline, audio_queue, args.plot), interval=30, blit=True)
+            if args.plot:
+                plt.show()
                 
     except KeyboardInterrupt:
         print("Done processing live data")
---
+++ final-project/audioProcessor.py
@@ -0,0 +1,54 @@
+import numpy as np
+import scipy as sp
+import librosa as lb
+
+
+class AudioProcessor:
+    def __init__(self, blocksize=256, samplerate=4000):
+        self.pitch = 0
+        self.amp = 0
+        self.mfcc = None
+        self.hanningWindow = np.hanning(blocksize)
+
+        self.n_fft_internal = blocksize * 2 - 1
+        self.melFilters = lb.filters.mel(sr=samplerate, n_fft=self.n_fft_internal, n_mels=13)
+        self.x_fft = np.fft.rfftfreq(self.n_fft_internal, 1/samplerate)
+
+
+    def CalcMFCC(self, soundData: np.ndarray) -> tuple[np.ndarray,np.ndarray, int, int]:
+        fft = np.abs(np.fft.rfft(soundData.flatten(), n=len(self.hanningWindow)*2 - 1))
+
+        argmax_fft = np.argmax(fft)
+        pitch = self.x_fft[argmax_fft] # Calculates the pitch
+        amplitude = fft[argmax_fft]
+
+        # Calculates the mfcc 
+        hanningFft = fft * self.hanningWindow
+        melFft = np.dot(self.melFilters, hanningFft)
+        dctFft = sp.fftpack.dct(melFft, type=2)
+
+        return hanningFft, dctFft, pitch, amplitude
+
+    def update_plot(self, frame, line, vline, audio_queue, plot=False):
+        soundData = None
+        while not audio_queue.empty():
+            soundData = audio_queue.get()
+
+        if soundData is not None:
+            # Process data
+            fft, mfcc, pitch, amp = self.CalcMFCC(soundData)
+            
+            # Store values for other functions to use
+            self.mfcc = mfcc
+            self.pitch = pitch
+            self.amp = amp
+
+            if plot:
+                line.set_ydata(fft)
+                vline.set_xdata([pitch, pitch])
+            
+        return line, vline
+
+    def other_function(self):
+        # This function can access the latest data whenever it wants
+        print(f"Current Pitch: {self.pitch}, Amplitude: {self.amp}")
